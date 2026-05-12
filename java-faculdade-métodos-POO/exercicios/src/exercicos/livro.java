7d62146a659865275e036e5c7d5ba71376aff77d
Matheus da Silveira Putrique <145862549+Matheus-Putrique@users.noreply.github.com>
2026-05-11 22:28:21 -0300

    Delete java-faculdade-métodos-POO/exercicios directory

--- java-faculdade-métodos-POO/exercicios/src/exercicos/livro.java
+++
@@ -1,9 +0,0 @@
-public class livro {
-    String titulo;
-    String autor;
-    int ano;
-
-    public void exibirInfo(){
-            System.out.println("O livro " + this.titulo + " tem como autor " + this.autor + " e foi publicado no ano de " + this.ano);
-    }
-}
